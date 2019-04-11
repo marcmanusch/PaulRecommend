d484bd87d5907c51738b76148298a31291ec39e9
Marc <marcdani@live.de>
2019-04-11 12:15:59 +0200

    ADD: s_articles_attributes new attr for calculated recommended articles

--- PaulRecommend.php
+++ PaulRecommend.php
@@ -11,6 +11,7 @@ namespace PaulRecommend;
 use Shopware\Components\Plugin;
 use Shopware\Components\Plugin\Context\InstallContext;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Shopware\Components\Plugin\Context\UninstallContext;
 
 class PaulRecommend extends Plugin
 {
@@ -20,6 +21,8 @@ class PaulRecommend extends Plugin
      */
     public function install(InstallContext $context)
     {
+        $service = $this->container->get('shopware_attribute.crud_service');
+        $service->update('s_articles_attributes', 'recommend_articles', 'string');
     }
 
     /**
@@ -29,6 +32,12 @@ class PaulRecommend extends Plugin
     {
         $container->setParameter('paul_recommend.plugin_dir', $this->getPath());
         parent::build($container);
+    }
+
+    public function uninstall(UninstallContext $context)
+    {
+        $service = $this->container->get('shopware_attribute.crud_service');
+        $service->delete('s_articles_attributes', 'recommend_articles');
     }
 
 }
